0b98ce21ad92af0d0a796be8cc521cd4a8c4acb1
Gislaine Mendes <gislainemrocha@gmail.com>
2021-09-07 21:58:35 -0300

    Adding all files

---
+++ src/br/com/alura/bookstore/teste/Teste.java
@@ -0,0 +1,25 @@
+package br.com.alura.bookstore.teste;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import br.com.alura.bookstore.modelo.Autores;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Autores a = new Autores();
+		a.setName("Jorge Mateus");
+		a.setBirthDate(LocalDate.of(1980, 3, 21));
+		a.setEmail("jorge@gmail.com");
+		a.setMiniCurriculo("Eu sou um autor de livros");
+		
+		System.out.println(a.getName());
+		System.out.println(a.getBirthDate().format(DateTimeFormatter
+				.ofPattern("dd/MM/yyyy")));
+		System.out.println(a.getEmail());
+		System.out.println(a.getMiniCurriculo());
+
+	}
+
+}
