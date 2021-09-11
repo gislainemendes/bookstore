af8ed002dbec49bc8371cd7b30bc4dc8887001b0
Gislaine Mendes <gislainemrocha@gmail.com>
2021-09-11 18:52:16 -0300

    DAO servlet

--- src/br/com/alura/bookstore/servlet/AutoresServlet.java
+++ src/br/com/alura/bookstore/servlet/AutoresServlet.java
@@ -1,11 +1,14 @@
 package br.com.alura.bookstore.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.alura.bookstore.dao.AutoresDAO;
+import br.com.alura.bookstore.factory.ConnectionFactory;
 import br.com.alura.bookstore.modelo.Autores;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,13 +18,19 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/autores")
 public class AutoresServlet extends HttpServlet {
+	
+	private AutoresDAO dao;
 
-	private List<Autores> autores = new ArrayList<>();
+	public AutoresServlet() {
+		Connection conexao = new ConnectionFactory().getConnection();
+		this.dao = new AutoresDAO(conexao);
+	}
+
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.
-		setAttribute("autores", autores);
+		setAttribute("autores", dao.listar());
 		
 		req
 		.getRequestDispatcher("WEB-INF/jsp/autores.jsp")
@@ -38,12 +47,8 @@ public class AutoresServlet extends HttpServlet {
 				DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		String curriculo = req.getParameter("curriculo");
 		
-		Autores a1 = new Autores(
-				nome,
-				email,
-				data, curriculo);
-		
-		autores.add(a1);
+		Autores novo = new Autores(nome, email, data, curriculo);
+		dao.salvar(novo);
 
 		resp.sendRedirect("autores");
 	}
